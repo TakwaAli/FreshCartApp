1c339ae03da3075950be9e8bea93bbddc9f61c56
Takwa <takwaali1520@gmail.com>
2023-04-20 01:19:59 +0200

    product details slider

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -3,9 +3,17 @@ import styles from './ProductDetails.module.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { Button } from 'bootstrap';
+import Slider from "react-slick";
 
 export default function ProductDetails() {
+// slider 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
 
   const [productDetails, setproductDetails] = useState([]);
   const param = useParams()
@@ -26,16 +34,24 @@ export default function ProductDetails() {
   },[])
   return (
   <>
+
   <div className="container">
-    <div className="row">
-      <div className='col-md-3 mt-4'>
-        <img className='w-100' src={productDetails.imageCover} alt="" />
+    <div className="row align-items-center">
+      <div className='col-md-4 '>
+        {/* <img className='w-100' src={productDetails?.imageCover} alt="" /> */}
+
+        <Slider {...settings}>
+       {(productDetails.images)?.map((imag)=>{return(<>
+        <img className='w-100' src={imag}/>
+       </>)})}
+
+        </Slider>
       </div>
-      <div className='offset-1 col-md-8 mt-5'>
-        <h2 className='mt-2'>{productDetails.title}</h2>
-        <p className='mt-2'>{productDetails.description}</p>
-        <h4 className='mt-2'>price : {productDetails.price}EGP</h4>
-        <h4 className='mt-2'>Quantity : {productDetails.quantity}</h4>
+      <div className='col-md-8 mt-5'>
+        <h2 className='mt-2'>{productDetails?.title}</h2>
+        <p className='mt-2'>{productDetails?.description}</p>
+        <h4 className='mt-2'>price : {productDetails?.price}EGP</h4>
+        <h4 className='mt-2'>Quantity : {productDetails?.quantity}</h4>
         <button className='fs-5 btn bg-main text-white w-100 mt-3 '>+ Add To Cart</button>
       </div>
     </div>
---
+++ src/Components/ProductDetailsSlider/ProductDetailsSlider.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ProductDetailsSlider.module.css';
+import Slider from "react-slick";
+export default function ProductDetailsSlider() {
+  return (
+  <>ProductDetailsSlider</>
+  )
+}
