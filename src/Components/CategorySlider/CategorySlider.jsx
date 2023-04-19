415d5210d5bc16f44a3d2dc4dcd7ef7064841c57
Takwa <takwaali1520@gmail.com>
2023-04-20 01:53:06 +0200

    categories slider

--- src/Components/CategorySlider/CategorySlider.jsx
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+
 import styles from './CategorySlider.module.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Slider from "react-slick";
 export default function CategorySlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 4
+  };
+  const [categories, setcategories] = useState([]);
+  async function Getcategories() {
+   let {data}= await axios.get(`https://route-ecommerce.onrender.com/api/v1/categories`)
+   setcategories(data.data)
+  // console.log(data.data);
+  }
+  useEffect(()=>{
+      Getcategories()
+  },[])
   return (
-  <>CategorySlider</>
+  <>
+  <Slider {...settings}>
+       {categories.map((category)=>
+       {return(<>
+        <div key={category._id}>
+          <img height={200} className='w-100' src={category.image} alt="" />
+          <h2 className='h5 text-center'>{category.name}</h2>
+        </div>
+       </>)})}
+
+        </Slider>
+  </>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Home.module.css';
 import FeatureProduct from '../FeatureProduct/FeatureProduct';
+import CategorySlider from '../CategorySlider/CategorySlider';
 export default function Home() {
   return (
     
   <>
+  <CategorySlider></CategorySlider>
  <FeatureProduct></FeatureProduct>
   </>
   )
