07ed7235005bf3ecdd2b449ff123844641886dea
Takwa <takwaali1520@gmail.com>
2023-04-26 23:15:09 +0200

    shop cart update design

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -32,10 +32,19 @@ export default function Cart() {
     <div className='col-md-1'>
 <img src={product.product.imageCover} className='w-100' alt="" />
     </div>
-    <div className='col-md-11'>
+    <div className='col-md-11 d-flex justify-content-between'>
+     <div>
      <h6>{product.product.title}</h6>
-     <h6>price: {product.price}</h6>
+     <h6 className='text-main'>price: {product.price}</h6>
+     <button className='btn m-0 p-z'><i className='fa-regular fa-trash-can text-danger'></i> Remove</button>
+     </div>
+     <div>
+      <button className='btn border-main btn-sm'>+</button>
+      <span className='mx-2'>{product.count}</span>
+      <button className='btn border-main btn-sm'>-</button>
+     </div>
     </div>
+
 </div>)}
   </div> 
 
